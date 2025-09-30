64ee3f678fd29cdfdda3b0ae0e5b3dab534a1211
Norm4-Igor <igorvguedesoliveira@gmail.com>
2025-09-30 11:08:04 -0300

    prints principais do projeto

--- sistema_de_controle_de_transito.py
+++ sistema_de_controle_de_transito.py
@@ -7,3 +7,8 @@ pagemento = input()
 infracao_leve = 130.16
 infracao_grave = 195.23
 infracao_gravissima = 880.41
+
+print("Placa:", placa_do_veiculo)
+print("Motorista:", nome_do_motorista)
+print("Velocidade registrada:", velocidade_registrada, "km/h")
+print("Velocidade máxima permitida:", velocidade_maxima_permitida, "km/h")
